66e5e2933d971f132389ba2528004cccbc308705
Jessuir Cleydson <jessuircleydson@gmail.com>
2017-11-02 23:58:15 -0300

    starting react-todo for sudy purposes

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/App.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React ToDo',
+      filename: 'index.html',
+      template: './index.html'
+    })
+  ],
+  devServer: {
+    contentBase: './dist'
+  },
+
+}
